05bbff21c53c6e711d93fbfb017af350d3e5ea91
Nesurr <nesurr7@gmail.com>
2020-09-07 20:40:48 +0300

    Практика 1 , Задание 5 Вывод ограничен 2 знаками после запятой

---
+++ src/classes/Main.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int nums=10;
+        for(int i=1;i<=nums;i++)
+        {
+            System.out.printf("%.2f %s",1.0/i,"  ");//Ограниченны 2 знаками после запятой
+        }
+    }
+}
